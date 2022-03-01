1fe78e2138e103f8b4d56350d424a1f0c00b870c
unknown <mathiasimagine@gmail.com>
2022-02-28 21:42:11 -0300

    feat add component City

--- my-app/src/components/City.jsx
+++ my-app/src/components/City.jsx
@@ -1,10 +1,23 @@
 import React from "react";
+import {useParams} from "react-router-dom";
 
-
-export default function City(){
-    return(
-        <div>
-            City
+export default function City({onFilter}) {
+    const {id} = useParams();
+    console.log(id)
+    const city = onFilter(id)
+    return (
+        <div >
+                <div >
+                    <h2>{city.name}</h2>
+                    <div >
+                        <div>Temperatura: {city.temp} ºC</div>
+                        <div>Clima: {city.weather}</div>
+                        <div>Viento: {city.wind} km/h</div>
+                        <div>Cantidad de nubes: {city.clouds}</div>
+                        <div>Latitud: {city.latitud}º</div>
+                        <div>Longitud: {city.longitud}º</div>
+                    </div>
+            </div>
         </div>
     )
-};
+}
