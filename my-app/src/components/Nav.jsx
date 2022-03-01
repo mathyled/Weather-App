44e7d250a3aa2ffc3bfa4db27606cb3fd4fbe88e
unknown <mathiasimagine@gmail.com>
2022-02-28 21:42:34 -0300

    feat add component Nav

--- my-app/src/components/Nav.jsx
+++ my-app/src/components/Nav.jsx
@@ -1,10 +1,27 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import SearchBar from "./SearchBar";
+import style from "./Nav.module.css";
 
-export default function Nav({onSearch}){
-    return(
-        <div>
-         <SearchBar onSearch={onSearch} />
-        </div>
+
+
+export default function Nav({ onSearch }) {
+    return (
+        <nav className={style.nav} >
+            <div>
+                <Link to="/" className={style.link}  >
+                    <span>Home</span>
+                </Link>
+            </div>
+            <div>
+                <Link to='/about' className={style.link} >
+                    <span>About</span>
+                </Link>
+            </div>
+            <div>
+
+                <SearchBar onSearch={onSearch} />
+            </div>
+        </nav>
     )
 };
