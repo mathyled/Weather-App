0977748bd708a9dc08af063f06e894947c9d29a4
unknown <mathiasimagine@gmail.com>
2022-02-28 21:43:40 -0300

    feat add component SearchBar

--- my-app/src/components/SearchBar.jsx
+++ my-app/src/components/SearchBar.jsx
@@ -1,21 +1,27 @@
-import React from "react";
-import style from "./SearchBar.module.css"
-import lupa from "../assets/lupa.png";
-import { FaSearchLocation } from 'react-icons/fa';
+import React, { useState } from "react";
+import styles from "./SearchBar.module.css";
 
-export default function SearchBar({onSearch}){
-    return(
-        <div className={style.root}>
-            <input
-            type="text"
-            className={style.input}
-            placeholder="City.."
-            ></input>
-        <button
-         type="submit" 
-         className={style.btn}
-         onClick={()=> onSearch("Searching..")}   
-         ><FaSearchLocation/></button>
-        </div>
-    )
-};
+export default function SearchBar({onSearch}) {
+  const [city,setCity] = useState("");
+
+  const handleInputChange= (e)=>{
+    e.preventDefault();
+    setCity(e.target.value)
+  }
+  //cuando haya un cambio en el input lo detecte mediante el listener onChange 
+  return (
+    <form onSubmit={(e) => {
+      e.preventDefault();
+      onSearch(city)}}
+    className={styles.root}
+    >
+      <input
+        type="text"
+        placeholder="Ciudad..."
+        onChange={(e)=> handleInputChange(e)}
+        className={styles.input}
+      />
+      <input type="submit" value="Search"  className={styles.btn} />
+    </form>
+  );
+}
